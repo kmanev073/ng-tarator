104301fad6b6e27f6481fd6a5174341d93ddb0f7
Kaloyan Manev <kaloyan.manev@lights.digital>
2021-06-23 09:52:34 +0300

    Improve components in test app

--- projects/ng-tarator-test-app/src/app/app-routing.module.ts
+++ projects/ng-tarator-test-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './containers/home/home.component';
+import { TestComponent } from './containers/test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'test', component: TestComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/ng-tarator-test-app/src/app/app.module.ts
+++ projects/ng-tarator-test-app/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './containers/app/app.component';
+import { HomeComponent } from './containers/home/home.component';
+import { TestComponent } from './containers/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
